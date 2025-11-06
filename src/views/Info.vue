0439573f96c46384ee34d20ab55a61811fb832b0
pokram <74709464+pokram@users.noreply.github.com>
2025-11-06 14:52:29 +0200

    Update 1 Info.vue

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -3,7 +3,26 @@
 </script>
 
 <template>
+    <section class="min-h-screen w-full bg-cover bg-center flex items-start justify-center p-12 bg-[url('src/assets/bg.jpg')]">
+    
+    <div class="max-w-5xl text-center font-oswald bg-black p-10 rounded-[20px]">
+      <!-- Pealkiri -->
+      <h1 class="mt-4 leading-tight font-medium text-[72px] tracking-wide text-[#F3C17A]">
+        KOOSTA BURGER<br />
+        ENDA MAITSE JÄRGI
+      </h1>
 
-    <h1 class="text-red-500">Primo Burger Info Page</h1>
-    <h6>More details coming soon...</h6>
+      <!-- Burger gif -->
+      <div class="mt-8">
+            <img src="../assets/burger.gif" alt="Koosta enda burger" class="mx-auto max-w-2xl w-full h-auto rounded-lg" />
+      </div>
+
+      <!-- List  -->
+      <ul class="mt-12 space-y-2 text-[32px] leading-snug text-[#F3C17A] text-left mx-auto w-fit">
+        <li class="list-none">• KOMPLEKTEERI ENDA LEMMIK KOMPONENTIDEST</li>
+        <li class="list-none">• SALVESTA LEMMIKUTESSE VÕI TELLI KOHE</li>
+      </ul>
+    </div>
+    
+  </section>
 </template>
