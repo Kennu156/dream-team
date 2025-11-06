c4b12f59c8bfd61ca704f1c0f7a12fdcc0877cf2
r0b1nv41kl4 <robin.vaikla@ametikool.ee>
2025-11-06 14:52:57 +0200

    Update Comingsoon.vue

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -4,6 +4,13 @@
 </script>
 
 <template>
-    <h1 class="text-red-500">Primo Burger</h1>
-    <h6>Coming Soon</h6>
+    <section class="bg-[url('src/assets/bg.jpeg')] bg-cover bg-center h-screen pt-2 font-oswald font-medium">
+            <h1 class="font-oswald font-medium font-display text-center text-orange-500">Primo Burger</h1>
+            <img class="pt-3 w-55 h-45 mx-auto" src="/src/assets/logo.png">
+            <h6 class="text-[60px] pt-5 text-center text-[#F3C17A]">COMING SOON</h6>
+            <h6 class="text-[50px] pt-12 text-center text-[#F3C17A]">VEEBIÄPPI AVAME</h6>
+            <h6 class="text-[50px] text-center text-[#F3C17A]">24.12.2025</h6>
+            <h6 class="text-[20px] mt-20 text-center text-[#F3C17A]">Skrolli alla!</h6>
+            <img class="pt-2 size-4 object-bottom block mx-auto h-auto" src="/src/assets/arrow.svg">
+    </section>
 </template>
